fa1eab5d6d1128671dc66fe6f928c3aa7729cc02
SAI <sai@SAIdeMacBook-Pro.local>
2017-03-23 01:31:38 +0800

    add user_id to group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
+  belongs_to :user
   validates :movie, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322170908) do
+ActiveRecord::Schema.define(version: 20170322172831) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "movie"
     t.text     "review"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
